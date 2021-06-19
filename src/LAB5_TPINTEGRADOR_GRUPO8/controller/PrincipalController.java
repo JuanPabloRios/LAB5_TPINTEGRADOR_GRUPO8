d665721e1f06d92bbb0060f01e1f7e12aa7d9556
rios.ezequielm@gmail.com <rios.ezequielm@gmail.com>
2021-06-19 15:50:49 -0300

    Hibernate commit  tabla usuarios yy tipos de usuarios

--- src/LAB5_TPINTEGRADOR_GRUPO8/controller/PrincipalController.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/controller/PrincipalController.java
@@ -1,11 +1,56 @@
 package LAB5_TPINTEGRADOR_GRUPO8.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeUsuarios;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Usuario;
+
 @Controller
 public class PrincipalController {
+		
+	
+	public PrincipalController() {
+		SessionFactory sessionFactory;
+		//instancia de configuration(para configurar)
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		
+		//con el getproperties obtenemos los datos del xml para crear la instancia de serviceRegistry
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+		
+		//construimos una sessionFactory mediante el configuration, que es una sesion hacia la DB 
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+		//realizar consultas.
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		
+		TiposDeUsuarios tpCliente = new TiposDeUsuarios();
+		tpCliente.setDescripcion("Cliente");
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Ezequiel");
+		usuario.setApellido("Rios");
+		
+		usuario.setTipoDeUsuario(tpCliente);
+		
+		session.save(tpCliente);
+		session.save(usuario);
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		sessionFactory.close();
+	}
+	
 	@RequestMapping("redireccionar_Principal.html")
 	public ModelAndView redireccionarAPrincipal() {
 		ModelAndView mv = new ModelAndView();

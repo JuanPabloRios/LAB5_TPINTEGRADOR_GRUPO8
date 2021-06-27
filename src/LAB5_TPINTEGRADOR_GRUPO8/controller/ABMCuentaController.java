9154b79e205f67b9ea2f923abd99a183419c08e0
rios.ezequielm@gmail.com <rios.ezequielm@gmail.com>
2021-06-27 15:45:31 -0300

    cree el Selector de TIpoDeCuentas, las cuentas ya se pueden modificar, tienen los respectivos carteles de confirmacion, y devolucion de error en caso de algun problema. falta hacer que los tipos de cuenta en el dropdown los obtenga desde la base si se quisiera.

--- src/LAB5_TPINTEGRADOR_GRUPO8/controller/ABMCuentaController.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/controller/ABMCuentaController.java
@@ -1,8 +1,20 @@
 package LAB5_TPINTEGRADOR_GRUPO8.controller;
 
+import java.sql.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView; 
+import org.springframework.web.servlet.ModelAndView;
+
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Cuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeCuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
+import LAB5_TPINTEGRADOR_GRUPO8.selector.CuentaSelector;
+import LAB5_TPINTEGRADOR_GRUPO8.selector.TipoDeCuentaSelector;
+import LAB5_TPINTEGRADOR_GRUPO8.service.CuentaService;
 
 @Controller
 public class ABMCuentaController {
@@ -14,4 +26,46 @@ public class ABMCuentaController {
         mv.setViewName("BuscarCliente");
         return mv;
     }
+	
+	
+	
+	
+	  @RequestMapping("editCuenta.html")
+	    public ModelAndView editarCuenta(String nombreCuenta, Double saldo, String CBU, Date fechaCuenta, String tipoCuenta, Integer numeroCuenta) {
+	        ModelAndView mv = new ModelAndView(); 
+	        
+	        mv.addObject("nombreCuenta",nombreCuenta); 
+	        TiposDeCuentas tpCuentas = TipoDeCuentaSelector.obtenerTipoCuentaPorNombre(tipoCuenta);
+	        
+	        String result = CuentaService.editarCuenta(numeroCuenta, saldo, tpCuentas ); 
+
+	        if(result.equalsIgnoreCase("OK")) {
+				
+		        mv.addObject("listaCuentas",CuentaSelector.obtenerTodasLasCuentas()); 
+		        mv.addObject("informarCuentaEditada",true);
+		        mv.addObject("nombreCuenta",nombreCuenta); 
+		        mv.setViewName("ListarCuentas");
+	        } else {
+	        	ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class); 
+	        	Cuentas cuenta = (Cuentas)appContext.getBean("cuenta"); 
+	        	
+	        	cuenta.setSaldo(saldo);;
+	        	cuenta.setCBU(CBU);
+	        	cuenta.setTipoCuenta(tpCuentas);
+	        	cuenta.setFechaCreacion(fechaCuenta);
+	        	
+	        	
+	        	mv.addObject("cliente",cuenta);
+	        	mv.addObject("informarError",true);
+	        	mv.addObject("mensajeError",result); 
+	        	mv.setViewName("ABMCuenta"); 
+	        	((ConfigurableApplicationContext)appContext).close();
+	        }  
+			
+			
+	        return mv;
+	    }
+	
+	
+	
 }
--- src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
@@ -1,5 +1,45 @@
 package LAB5_TPINTEGRADOR_GRUPO8.service;
 
-public class CuentaService {
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Cuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeCuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
+import LAB5_TPINTEGRADOR_GRUPO8.selector.ConfigHibernate;
+import LAB5_TPINTEGRADOR_GRUPO8.selector.CuentaSelector;
+
+public class CuentaService {
+	
+	  public static String editarCuenta(Integer numeroCuenta, Double saldo , TiposDeCuentas tipoCuenta){ 
+	    	try {
+	    		
+	    		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class);
+		     	Cuentas ca = CuentaSelector.obtenerCuentaPorId(numeroCuenta);	     	
+		     	ConfigHibernate config = new ConfigHibernate();
+		    	Session session = config.abrirConexion();
+		    	
+		    	session.beginTransaction(); 
+		    	
+		    	if(!ca.getSaldo().equals(saldo)) { 
+		    		ca.setSaldo(saldo);
+		    	}
+		    	if(ca.getTipoCuenta().getDescripcion() != tipoCuenta.getDescripcion()) {
+		    		ca.setTipoCuenta(tipoCuenta);
+		    	}
+	
+		    	session.update(ca); 
+		    	session.getTransaction().commit(); 
+		    	config.cerrarSession(); 
+		    	((ConfigurableApplicationContext)appContext).close();
+	        	return "OK";
+		    }catch (HibernateException he){
+		        he.printStackTrace();
+		        return "Ocurrio una excepcion durante la Modificacion";
+		    }
+	    }	
 }

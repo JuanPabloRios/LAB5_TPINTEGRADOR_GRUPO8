6a343875fba7c02df8173c69f3a5eb19beb74596
Rocio <rocioafavre@gmail.com>
2021-06-28 17:04:17 -0300

    Alta Cuentas

--- src/LAB5_TPINTEGRADOR_GRUPO8/controller/ABMCuentaController.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/controller/ABMCuentaController.java
@@ -15,6 +15,7 @@ import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
 import LAB5_TPINTEGRADOR_GRUPO8.selector.CuentaSelector;
 import LAB5_TPINTEGRADOR_GRUPO8.selector.TipoDeCuentaSelector;
 import LAB5_TPINTEGRADOR_GRUPO8.service.CuentaService;
+import LAB5_TPINTEGRADOR_GRUPO8.service.UsuarioService;
 
 import java.util.List; 
 import org.springframework.stereotype.Controller;
@@ -74,4 +75,35 @@ public class ABMCuentaController {
 			
 	        return mv;
 	    } 
+	  
+	    //guardarCuenta.html 
+	    @RequestMapping("guardarCliente.html")
+	    public ModelAndView guardarCuenta(String nombreCuenta, Double saldo, String CBU, Date fechaCuenta, String tipoCuenta, Integer numeroCuenta ) {
+	        ModelAndView mv = new ModelAndView(); 
+	        mv.addObject("nombreCuenta",nombreCuenta);
+	        TiposDeCuentas tpCuentas = TipoDeCuentaSelector.obtenerTipoCuentaPorNombre(tipoCuenta);
+	        
+	        String result = CuentaService.crearCuenta( nombreCuenta, saldo, CBU,fechaCuenta, tipoCuenta, numeroCuenta );
+	       
+	        if(result.equalsIgnoreCase("OK")) {
+
+	        	mv.addObject("listaCuentas",CuentaSelector.obtenerTodasLasCuentas()); 
+	        	mv.setViewName("AdministradorHome");
+	        } else {
+	        	ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class); 
+	        	Cuentas cuenta = (Cuentas)appContext.getBean("cuenta"); 
+	        	
+	        	cuenta.setSaldo(saldo);;
+	        	cuenta.setCBU(CBU);
+	        	cuenta.setTipoCuenta(tpCuentas);
+	        	cuenta.setFechaCreacion(fechaCuenta);
+	        	
+	        	mv.addObject("Cuenta",cuenta);
+	        	mv.addObject("informarError",true);
+	        	mv.addObject("mensajeError",result); 
+	        	mv.setViewName("ABMCuenta"); 
+	        	((ConfigurableApplicationContext)appContext).close();
+	        }  
+	        return mv;
+	    }
 }
--- src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
@@ -1,6 +1,8 @@
 package LAB5_TPINTEGRADOR_GRUPO8.service;
 
 
+import java.sql.Date;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.context.ApplicationContext;
@@ -9,9 +11,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.Cuentas;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeCuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Usuario;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
 import LAB5_TPINTEGRADOR_GRUPO8.selector.ConfigHibernate;
 import LAB5_TPINTEGRADOR_GRUPO8.selector.CuentaSelector;
+import LAB5_TPINTEGRADOR_GRUPO8.selector.TipoDeUsuarioSelector;
 
 public class CuentaService {
 	
@@ -41,5 +45,47 @@ public class CuentaService {
 		        he.printStackTrace();
 		        return "Ocurrio una excepcion durante la Modificacion";
 		    }
-	    }	
+	    }
+
+	public static String crearCuenta(String nombreCuenta, Double saldo, String CBU, Date fechaCuenta, String tipoCuenta, Integer numeroCuenta){ 
+	    	try{
+		    	Boolean limiteCantCuentas = CuentaService.limiteCuentas(); 
+		    	
+		    	if(!limiteCantCuentas) {
+		    		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class);
+		    		ConfigHibernate config = new ConfigHibernate();
+		        	Session session = config.abrirConexion();
+		        	session.beginTransaction();
+		        	
+		            Cuentas cu = (Cuentas)appContext.getBean("Cuenta"); 
+		            cu.setTipoDeUsuario(TipoDeUsuarioSelector.obtenerTipoUsuarioPorNombre("Cliente"));
+		            cu.setNombre(nombreCliente);
+		            cu.setApellido(apellidoCliente);
+		            cu.setContrasenia(contrasenia);
+		            cu.setUsuario(nombreUsuario);   
+		            cu.setDireccion(direccionCliente);
+		            cu.setDNI(dniCliente); 
+		            cu.setNacionalidad(nacionalidadCliente);
+		            cu.setSexo(sexoCliente);
+		            cu.setFecha_de_nacimiento(fechaNacimientoCliente);   
+		            session.save(cu);  
+		        	session.getTransaction().commit(); 
+		        	config.cerrarSession();
+
+		        	((ConfigurableApplicationContext)appContext).close();
+		        	return "OK";
+		    	} else {
+		    		return "El cliente tiene un limite de cuatro cuentas";
+		    	}
+		    }catch (HibernateException he){
+		        he.printStackTrace();
+		        return "Ocurrio una excepcion durante el guardado";
+		    } 
+		
+	}
+
+	private static Boolean limiteCuentas() {
+		// TODO 
+		return null;
+	}	
 }

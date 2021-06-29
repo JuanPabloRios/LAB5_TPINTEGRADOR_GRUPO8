5770d528fdb31863ffe0218ba6785dc9f5546dff
Saura-Matias <81603801+Saura-Matias@users.noreply.github.com>
2021-06-29 11:13:32 -0300

    CuentaService-CuentaDao
    Intento de traspaso de control de excepciones CuentaService a CuentaDao, si está correcto se procederá a realizar lo mismo con el resto de los DAO/Service (subo los archivos por separado, directo al repo en el sitio web porque github desktop me trajo bastantes problemas)

--- src/LAB5_TPINTEGRADOR_GRUPO8/dao/CuentaDao.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/dao/CuentaDao.java
@@ -1,6 +1,8 @@
 package LAB5_TPINTEGRADOR_GRUPO8.dao; 
 import java.util.ArrayList; 
-import java.util.List; 
+import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,12 +20,19 @@ public class CuentaDao {
         Session se = ch.abrirConexion(); 
         List<Cuentas> cuentasClientes = (List<Cuentas>)se.createQuery("FROM Cuentas").list(); 
         List<Cuentas> result = new ArrayList<>();
-        for(Integer i = 0; i< cuentasClientes.size(); i++) { 
-            if(cuentasClientes.get(i).getUsuario().getIdusuario() ==  usuarioId) {
-                result.add(cuentasClientes.get(i));
-            }
+        try {
+	        for(Integer i = 0; i< cuentasClientes.size(); i++) { 
+	            if(cuentasClientes.get(i).getUsuario().getIdusuario() ==  usuarioId) {
+	                result.add(cuentasClientes.get(i));
+	            }
+	        }
         }
+        catch(HibernateException he){
+        	he.printStackTrace();
+        }
+        finally {
         ch.cerrarSession();
+        }
         return result;
     }
     
@@ -33,28 +42,46 @@ public class CuentaDao {
         Session se = ch.abrirConexion(); 
         List<Cuentas> cuentasClientes = (List<Cuentas>)se.createQuery("FROM Cuentas").list(); 
         List<Cuentas> result = new ArrayList<>();
-        for(Integer i = 0; i < cuentasClientes.size(); i++) {  
-        	result.add(cuentasClientes.get(i)); 
+        try {
+	        for(Integer i = 0; i < cuentasClientes.size(); i++) {  
+	        	result.add(cuentasClientes.get(i)); 
+	        }
         }
+        catch(HibernateException he){
+        	he.printStackTrace();
+        }
+        finally {
         ch.cerrarSession();
+        }
         return result;
     }
     
     public static Cuentas obtenerCuentaPorId(Integer idCuenta) {
     	ConfigHibernate config = new ConfigHibernate();
     	Session session = config.abrirConexion(); 
+    	try {
     	session.beginTransaction();
     	Cuentas cuenta = (Cuentas)session.get(Cuentas.class,idCuenta); 
     	config.cerrarSession();
     	return cuenta;
+    	}
+    	catch(HibernateException he){
+        	he.printStackTrace();
+        }
+		return null;
     }
     
     public static Cuentas obtenerCuentaPorCBU(String cbu){ 
         List<Cuentas> cuentas = CuentaDao.obtenerTodasLasCuentas(); 
-        for(Integer i = 0; i< cuentas.size(); i++) { 
-            if(cbu.equals(cuentas.get(i).getCBU())) {
-                return cuentas.get(i);
-            }
+        try {
+	        for(Integer i = 0; i< cuentas.size(); i++) { 
+	            if(cbu.equals(cuentas.get(i).getCBU())) {
+	                return cuentas.get(i);
+	            }
+	        }
+        }
+        catch(HibernateException he){
+        	he.printStackTrace();
         }
         return null;
     }
@@ -62,10 +89,18 @@ public class CuentaDao {
     public static String actualizarCuenta(Cuentas ca) { 
      	ConfigHibernate config = new ConfigHibernate();
     	Session session = config.abrirConexion(); 
+    	try {
     	session.beginTransaction();
     	session.update(ca); 
     	session.getTransaction().commit(); 
-    	config.cerrarSession();  
+    	}
+    	catch(HibernateException he){
+        	he.printStackTrace();
+        	return "Ocurrio una excepcion durante la Modificacion";
+        }
+    	finally{
+    	config.cerrarSession();
+    	}
     	return "OK";
     }
     

a8a9da01c5745f436cf5c7c023f2a777c6b47213
NorevDurok <81711551+NorevDurok@users.noreply.github.com>
2021-06-29 19:54:10 -0300

    Codigo Baja CuentaService
    Y Agregue el codigo restante de Baja/Eliminar a CuentaService, necesito que me confirmen que todo esto funciona.

--- src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
@@ -8,12 +8,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import LAB5_TPINTEGRADOR_GRUPO8.dao.CuentaDao;
+import LAB5_TPINTEGRADOR_GRUPO8.dao.UsuarioDao;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.Cuentas;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeCuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Usuario;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.ConfigHibernate;
 
 public class CuentaService {
+	
+    public static void eliminarCuentaPorId(Integer idNroDeCuenta){ 
+    	Cuentas cuenta = CuentaDao.obtenerCuentaPorId(idNroDeCuenta);     	
+	    CuentaDao.eliminarCuenta(cuenta);
+    }
 	
 	  public static String editarCuenta(Integer numeroCuenta, Double saldo , TiposDeCuentas tipoCuenta){  
 		     	Cuentas ca = CuentaDao.obtenerCuentaPorId(numeroCuenta);	 

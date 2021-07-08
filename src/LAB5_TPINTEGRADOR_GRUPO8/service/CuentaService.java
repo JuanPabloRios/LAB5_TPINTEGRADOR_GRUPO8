ce707320bf38fd761c2d29d85d136c5582deda15
Saura-Matias <81603801+Saura-Matias@users.noreply.github.com>
2021-07-08 03:44:51 -0300

    Baja de cuentas asociadas a usuario eliminado
    Se crean métodos y llamadas a los mismos para que cuando se da de baja un usuario se den de baja las cuentas vinculadas al mismo

--- src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/service/CuentaService.java
@@ -1,6 +1,8 @@
 package LAB5_TPINTEGRADOR_GRUPO8.service;
 
 
+import java.sql.Date;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.context.ApplicationContext;
@@ -8,21 +10,92 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import LAB5_TPINTEGRADOR_GRUPO8.dao.CuentaDao;
+import LAB5_TPINTEGRADOR_GRUPO8.dao.UsuarioDao;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.Cuentas;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.TiposDeCuentas;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Usuario;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
+
+
+import LAB5_TPINTEGRADOR_GRUPO8.dao.CuentaDao;
+import LAB5_TPINTEGRADOR_GRUPO8.dao.TipoDeUsuarioDao;
+
 import LAB5_TPINTEGRADOR_GRUPO8.resources.ConfigHibernate;
 
 public class CuentaService {
+	 
+	public static void eliminarTodasLasCuentasDeClientePorId(Integer idUsuario){ 
+    	CuentaDao.eliminarTodasLasCuentasDeClientePorId(idUsuario);     	
+    }
 	
-	  public static String editarCuenta(Integer numeroCuenta, Double saldo , TiposDeCuentas tipoCuenta){  
-		     	Cuentas ca = CuentaDao.obtenerCuentaPorId(numeroCuenta);	 
-		    	if(!ca.getSaldo().equals(saldo)) { 
+	public static void eliminarCuentaPorId(Integer idNroDeCuenta){ 
+    	Cuentas cuenta = CuentaDao.obtenerCuentaPorId(idNroDeCuenta);     	
+	    CuentaDao.eliminarCuenta(cuenta);
+    }
+	 
+	  public static String editarCuenta(Integer numeroCuenta, Double saldo , TiposDeCuentas tipoCuenta, Integer idUsuario ){ 
+	    	try{ 
+		     	Cuentas ca = CuentaDao.obtenerCuentaPorId(numeroCuenta);
+		     	Usuario us = UsuarioDao.obtenerUsuarioPorID(idUsuario);
+		     	
+		    	Boolean actualizar = false;
+		    	
+		     	if(!ca.getSaldo().equals(saldo)) { 
 		    		ca.setSaldo(saldo);
+		    		actualizar = true;
 		    	}
+		    	
 		    	if(ca.getTipoCuenta().getDescripcion() != tipoCuenta.getDescripcion()) {
 		    		ca.setTipoCuenta(tipoCuenta);
+		    		actualizar = true;
 		    	}
-		    	return CuentaDao.actualizarCuenta(ca); 
-	    }	
+		    	
+		    	if(ca.getUsuario().getIdusuario() != us.getIdusuario()) {
+		    		ca.setUsuario(us);
+		    		actualizar = true;
+		    	}
+		    	
+		    	if(actualizar) {
+			    	CuentaDao.actualizarCuenta(ca); 
+		    	}
+	        	return "OK";
+		    }catch (HibernateException he){
+		        he.printStackTrace();
+		        return "Ocurrio una excepcion durante la Modificacion";
+		    }
+	    } 
+	    
+		public static String crearCuenta(Double saldo, String CBU, Date fechaCuenta,TiposDeCuentas tpCuenta,Integer numeroCuenta, Integer idUsuario ){ 
+	    	try{
+		    	Boolean limiteCantCuentas = false; // = CuentaService.limiteCuentas(); // FALTA ESTO!!! y qeu el saldo inicial es $10.000
+		    	
+		    	if(!limiteCantCuentas) {
+		    		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class); 
+		            Cuentas cuenta = (Cuentas)appContext.getBean("cuenta"); 
+		            Usuario usuario= UsuarioDao.obtenerUsuarioPorID(idUsuario);
+		           
+		        	
+		        	cuenta.setSaldo(saldo);
+		        	cuenta.setCBU(CBU);
+		        	cuenta.setTipoCuenta(tpCuenta);
+		        	cuenta.setFechaCreacion(fechaCuenta);
+		        	cuenta.setUsuario(usuario);
+		        	CuentaDao.insertarCuenta(cuenta);
+		        	((ConfigurableApplicationContext)appContext).close();
+		        	return "OK";
+		    	} else {
+		    		return "El cliente tiene un limite de cuatro cuentas";
+		    	}
+		    }catch (HibernateException he){
+		        he.printStackTrace();
+		        return "Ocurrio una excepcion durante el guardado";
+		    } 
+		
+	}
+
+	private static Boolean limiteCuentas() {
+		// TODO 
+		return null;
+	}	
+
 }
--- src/LAB5_TPINTEGRADOR_GRUPO8/service/UsuarioService.java
+++ src/LAB5_TPINTEGRADOR_GRUPO8/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package LAB5_TPINTEGRADOR_GRUPO8.service; 
 import LAB5_TPINTEGRADOR_GRUPO8.dao.TipoDeUsuarioDao;
 import LAB5_TPINTEGRADOR_GRUPO8.dao.UsuarioDao;
+import LAB5_TPINTEGRADOR_GRUPO8.entidad.Localidad;
 import LAB5_TPINTEGRADOR_GRUPO8.entidad.Usuario;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.Config;
 import LAB5_TPINTEGRADOR_GRUPO8.resources.ConfigHibernate;
@@ -18,14 +19,17 @@ public class UsuarioService {
     public static void eliminarUsuarioPorId(Integer idUsuario){ 
     	Usuario usuario = UsuarioDao.obtenerUsuarioPorID(idUsuario);     	
 	    UsuarioDao.eliminarUsuario(usuario);
+	    CuentaService.eliminarTodasLasCuentasDeClientePorId(idUsuario);
     }	
     
     //FALTA AGREGAR EL CAMPO DE LOCALIDAD Y PROVINCIA
     public static String crearUsuario(String nombreCliente, String apellidoCliente, Integer dniCliente, Date fechaNacimientoCliente, 
-    	String nacionalidadCliente, String direccionCliente, String sexoCliente, String provinciaCliente, String localidadCliente,
+    	String nacionalidadCliente, String direccionCliente, String sexoCliente, String provinciaCliente, Integer localidadCliente,
 		String nombreUsuario, String contrasenia){ 
     	Boolean existe = UsuarioService.existeDNI(dniCliente); 
 	    	if(!existe) {
+	    		Localidad loc = LocalidadService.obtenerLocalidadPorId(localidadCliente);
+	    		
 	    		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class); 
 	            Usuario us = (Usuario)appContext.getBean("UsuarioCliente"); 
 	            us.setTipoDeUsuario(TipoDeUsuarioDao.obtenerTipoUsuarioPorNombre("Cliente"));
@@ -38,6 +42,7 @@ public class UsuarioService {
 	            us.setNacionalidad(nacionalidadCliente);
 	            us.setSexo(sexoCliente);
 	            us.setFecha_de_nacimiento(fechaNacimientoCliente);  
+	            us.setLocalidad(loc);
 	            UsuarioDao.insertarUsuario(us);
 	        	((ConfigurableApplicationContext)appContext).close();
 	        	return "OK";
@@ -49,11 +54,47 @@ public class UsuarioService {
     
     //FALTA AGREGAR LOCALIDAD Y PROVINCIA
     public static String editarUsuario(Integer idUsuario, String nombreCliente, String apellidoCliente, Integer dniCliente, Date fechaNacimientoCliente, 
-			String nacionalidadCliente, String direccionCliente, String sexoCliente, String provinciaCliente, String localidadCliente,
+			String nacionalidadCliente, String direccionCliente, String sexoCliente, String provinciaCliente, Integer localidadCliente,
 			String nombreUsuario, String contrasenia){ 
-    	Boolean existe = UsuarioService.existeDNI(dniCliente); 
+    		Boolean existe = UsuarioService.existeDNI(dniCliente); 
+    		Usuario us = UsuarioDao.obtenerUsuarioPorID(idUsuario); 
+    		Localidad loc = LocalidadService.obtenerLocalidadPorId(localidadCliente);
 	    	if(!existe) { 
-		     	Usuario us = UsuarioDao.obtenerUsuarioPorID(idUsuario); 
+		    	if(!us.getNombre().contains(nombreCliente)) { 
+		    		us.setNombre(nombreCliente);
+		    	}
+		    	if(!us.getApellido().contains(apellidoCliente)) {
+		    		us.setApellido(apellidoCliente);
+		    	}
+		    	
+		    	if(!us.getContrasenia().contains(contrasenia)) {
+		    		us.setContrasenia(contrasenia);
+		    	}
+		    	if(!us.getUsuario().contains(nombreUsuario)) {
+		    		us.setUsuario(nombreUsuario);
+		    	}
+		    	
+		    	if(us.getDireccion() != direccionCliente) {
+		    		us.setDireccion(direccionCliente);
+		    	}
+		    	if(us.getDNI() != dniCliente) {
+		    		us.setDNI(dniCliente);
+		    	}
+		    	if(!us.getNacionalidad().contains(nacionalidadCliente)) {   	    
+		    		us.setNacionalidad(nacionalidadCliente);
+		    	}
+		    	if(!us.getSexo().contains(sexoCliente)) {    	    
+		    		us.setSexo(sexoCliente);
+		    	}
+		    	if(!us.getFecha_de_nacimiento().equals(fechaNacimientoCliente)) {
+		    		us.setFecha_de_nacimiento(fechaNacimientoCliente);  
+		    	}
+		    	if(us.getLocalidad().getIdLocalidad() != loc.getIdLocalidad()) {    	    
+		    		us.setLocalidad(loc);
+		    	}
+		    	UsuarioDao.actualizarUsuario(us); 
+	        	return "OK";
+	    	} else if(us.getDNI() == dniCliente) {
 		    	if(!us.getNombre().contains(nombreCliente)) { 
 		    		us.setNombre(nombreCliente);
 		    	}
@@ -83,9 +124,12 @@ public class UsuarioService {
 		    	if(!us.getFecha_de_nacimiento().equals(fechaNacimientoCliente)) {
 		    		us.setFecha_de_nacimiento(fechaNacimientoCliente);  
 		    	} 
+		    	if(us.getLocalidad().getIdLocalidad() != loc.getIdLocalidad()) {    	    
+		    		us.setLocalidad(loc);
+		    	}
 		    	UsuarioDao.actualizarUsuario(us); 
 	        	return "OK";
-	    	} else {
+	    	}else {
 	    		return "El DNI ingresado ya existe";
 	    	}
     }	
